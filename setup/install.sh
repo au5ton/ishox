2d58336d87453c4066992c17ea9cc39f0a51f2d0
Austin Jackson <themacphage@gmail.com>
2020-02-19 00:18:43 -0600

    updated reference

--- setup/install.sh
+++ setup/install.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 # build and install shadowsocks-libev
+curl https://raw.githubusercontent.com/au5ton/ishox/master/setup/build.sh | sh
 
 # install components for UI
 apk update && \
 apk upgrade && \
 apk add bash dialog && \
+echo "complete"
